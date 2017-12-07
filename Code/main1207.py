0767e150ea50576ef5c9678bfa31407a8d8750c7
renzhiyang <923251870@qq.com>
2017-12-07 15:08:26 +0800

    main 1207

--- Code/main1207.py
+++ Code/main1207.py
@@ -36,7 +36,7 @@ def run_training1():
 
     sparse_ab_batch = ab_batch * mask_batch_2channels
     replace_image = (ab_batch - sparse_ab_batch) + (index_ab_batch * mask_batch_2channels)
-    mask_batch = mask_batch_2channels[:, :, :, 0]
+    mask_batch = mask_batch_2channels[:, :, :, 0:1]
 
     out_ab_batch = model.built_network(replace_image, mask_batch)
     sess = tf.Session()
